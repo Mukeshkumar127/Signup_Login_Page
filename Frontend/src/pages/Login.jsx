4d962fa029ac22d2e383beb5d83db9facfe4d95c
Mukesh Kumar <149704738+Mukeshkumar127@users.noreply.github.com>
2025-07-05 12:26:51 +0530

    Update Login.jsx

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -27,7 +27,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url = "https://signup-login-page-backend-api.vercel.app/auth/login" ;
+            const url = `https://signup-login-page-backend-api.vercel.app/auth/login` ;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
